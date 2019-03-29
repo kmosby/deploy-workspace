db58e9505de32b10488c0c2f9e90f6654f0e1783
Ken Mosby <kennmos@cdw.com>
2019-03-29 11:19:26 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://9qlqpbdi59.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7kic53hmpe.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'kennmos@cdw.com'
